adc852b75ad78668566c367526d95df1d605413c
confectionunivers <confectionunivers@gmail.com>
2026-07-05 03:22:45 +0000

    fix: configuration build vercel entry.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'hybrid',
-	adapter: vercel({
-		functions: {
-			api: {
-				include: ['src/pages/api/**'],
-			},
-		},
-	}),
 	integrations: [react()],
 	vite: {
 		plugins: [tailwindcss()],
