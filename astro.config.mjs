4a1290d5c68663dd910f53eb4c0a366ac5eaf1d2
confectionunivers <confectionunivers@gmail.com>
2026-07-07 13:18:35 +0000

    fix: force la configuration functionPerRoute sur l'adaptateur vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,15 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: vercel(),
-	integrations: [react()],
-	vite: {
-		plugins: [tailwindcss()],
-	},
+    output: 'server',
+    adapter: vercel({
+        webAnalytics: { enabled: true },
+        imagesConfig: { sizes: [320, 640, 1280, 1920] },
+        // Forcer le déploiement en Serverless Function standard
+        functionPerRoute: false 
+    }),
+    integrations: [react()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
